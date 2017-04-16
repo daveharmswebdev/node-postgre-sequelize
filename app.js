79e45b1ab4a28c937c184060e989614ca83116f4
Dave Harms <daveharmswebdev@gmail.com>
2017-04-16 13:58:35 -0500

    added eslintrc and added get and post route for todos

--- app.js
+++ app.js
@@ -12,6 +12,8 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// require our routes into the application
+require('./server/routes')(app);
 app.get('*', (req, res) => {
   res.status(200).send({
     message: 'Welcome to the continuation of this wonderful journey'
